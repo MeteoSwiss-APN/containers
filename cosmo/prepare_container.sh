382b75f85a1aed38a62e29a74d171e47126d7110
cosunae <carlos.osuna@meteoswiss.ch>
2019-05-05 20:58:10 +0200

    fixes

--- cosmo/prepare_container.sh
+++ cosmo/prepare_container.sh
@@ -32,9 +32,7 @@ cd $workdir
 
 git clone git@github.com:C2SM-RCM/libgrib1.git
 git clone --branch libgrib_api_1.20.0.2 git@github.com:C2SM-RCM/libgrib-api-vendor.git libgrib-api
-cd libgrib-api
 git clone --branch v1.20.0.2 git@github.com:C2SM-RCM/libgrib-api-cosmo-resources.git cosmo_definitions
-cd $workdir
 git clone git@github.com:C2SM-RCM/libjasper.git
 
 if [[ "${ARCH}" == "GPU" ]]; then
